2e02214a711d9fefed4e4d72cd3d48822e462d1a
krs92 <sharath.k@transneuron.com>
2018-01-27 18:29:57 +0530

    formatting

--- Concurrency.go
+++ Concurrency.go
@@ -1,71 +1,70 @@
+package main
 
- package main
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
- import (
- "fmt"
- "math/rand"
- "sync"
- "time"
- )
+// wg is used to wait for the program to finish.
+var wg sync.WaitGroup
 
- // wg is used to wait for the program to finish.
- var wg sync.WaitGroup
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
- func init() {
- rand.Seed(time.Now().UnixNano())
- }
+// main is the entry point for all Go programs.
+func main() {
+	// Create an unbuffered channel.
+	court := make(chan int)
 
- // main is the entry point for all Go programs.
- func main() {
- // Create an unbuffered channel.
- court := make(chan int)
+	// Add a count of two, one for each goroutine.
+	wg.Add(2)
 
- // Add a count of two, one for each goroutine.
- wg.Add(2)
+	// Launch two players.
+	go player("Nadal", court)
+	go player("Djokovic", court)
 
- // Launch two players.
- go player("Nadal", court)
- go player("Djokovic", court)
+	// Start the set.
+	court <- 1
 
- // Start the set.
- court <- 1
-
- // Wait for the game to finish.
- wg.Wait()
- }
+	// Wait for the game to finish.
+	wg.Wait()
+}
 
 // sting 6.20 listing20.go
 // censed to Mark Watson <nordickan@gmail.com>
 // 0 CHAPTER 6 Concurrency
- // player simulates a person playing the game of tennis.
- func player(name string, court chan int) {
- // Schedule the call to Done to tell main we are done.
- defer wg.Done()
+// player simulates a person playing the game of tennis.
+func player(name string, court chan int) {
+	// Schedule the call to Done to tell main we are done.
+	defer wg.Done()
 
- for {
- // Wait for the ball to be hit back to us.
- ball, ok := <-court
- if !ok {
- // If the channel was closed we won.
- fmt.Printf("Player %s Won\n", name)
- return
- }
+	for {
+		// Wait for the ball to be hit back to us.
+		ball, ok := <-court
+		if !ok {
+			// If the channel was closed we won.
+			fmt.Printf("Player %s Won\n", name)
+			return
+		}
 
- // Pick a random number and see if we miss the ball.
- n := rand.Intn(100)
- if n%13 == 0 {
- fmt.Printf("Player %s Missed\n", name)
+		// Pick a random number and see if we miss the ball.
+		n := rand.Intn(100)
+		if n%13 == 0 {
+			fmt.Printf("Player %s Missed\n", name)
 
- // Close the channel to signal we lost.
- close(court)
- return
- }
+			// Close the channel to signal we lost.
+			close(court)
+			return
+		}
 
- // Display and then increment the hit count by one.
- fmt.Printf("Player %s Hit %d\n", name, ball)
- ball++
+		// Display and then increment the hit count by one.
+		fmt.Printf("Player %s Hit %d\n", name, ball)
+		ball++
 
- // Hit the ball back to the opposing player.
- court <- ball
- }
- }
+		// Hit the ball back to the opposing player.
+		court <- ball
+	}
+}
