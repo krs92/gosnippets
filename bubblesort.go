fc0dec6f8ff17e602c925b5fa57ceb93e6124639
sharath <krs.sharath03@gmail.com>
2018-08-30 12:43:27 +0530

    Update bubblesort.go

--- bubblesort.go
+++ bubblesort.go
@@ -3,28 +3,41 @@ package main
 import "fmt"
 
 func main() {
-	var numbers = []int{1, 2, 3, 4, 5}
-	fmt.Println("After 0 sweep(s):", numbers)
-	//sweep
-	sweep(numbers)
-	fmt.Println("After 1 sweep(s):", numbers)
+	var numbers = []int{20, 3, 34, 5, 6}
+	fmt.Println("Unsorted:", numbers)
 
+	bubbleSort(numbers)
+	fmt.Println("Sorted:", numbers)
 }
 
-func sweep(numbers []int) {
+func bubbleSort(numbers []int) {
 	var N = len(numbers)
-	fmt.Println(N)
-	var first = 0
-	var second = 1
-	for second < N {
-
-		var firstNumber = numbers[first]
-		var secondNumber = numbers[second]
-		if firstNumber > secondNumber {
-			numbers[first] = secondNumber
-			numbers[second] = first
+	var i int
+	for i = 0; i < N; i++ {
+		if !sweep(numbers, i) {
+			return
 		}
-		first++
-		second++
 	}
+}
+
+func sweep(numbers []int, prevPasses int) bool {
+	var N = len(numbers)
+	var firstIndex = 0
+	var secondIndex = 1
+	var didSwap = false
+
+	for secondIndex < (N - prevPasses) {
+		var firstNumber = numbers[firstIndex]
+		var secondNumber = numbers[secondIndex]
+
+		if firstNumber > secondNumber {
+			numbers[firstIndex] = secondNumber
+			numbers[secondIndex] = firstNumber
+			didSwap = true
+		}
+
+		firstIndex++
+		secondIndex++
+	}
+	return didSwap
 }
